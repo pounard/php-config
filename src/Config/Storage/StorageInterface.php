1c88072cfc2cc416cda3ec2312c3a18d3e7fae83
Pierre Rineau <pierre.rineau@makina-corpus.com>
2012-12-21 00:23:05 +0100

    Added StorageInterface::getLastStatus() and statuses constants. Added  parameter to delete() method.

--- src/Config/Storage/StorageInterface.php
+++ src/Config/Storage/StorageInterface.php
@@ -10,16 +10,54 @@ use Config\ConfigType;
  * Consider path here as being a raw string key, but please be careful with
  * tree handling, some methods needs the storage backend to be able to list
  * a complete specific subtree
+ *
+ * If any error happens in any method execution, the error code must be kept
+ * into memory until next method call. In case of success, error code must be
+ * reset to SUCCESS.
  */
 interface StorageInterface
 {
+    /**
+     * Last operation succeeded
+     */
+    const SUCCESS = 0;
+
+    /**
+     * Expected type mismatch the one stored
+     */
+    const ERROR_TYPE_MISMATCH = 10;
+
+    /**
+     * Given path is invalid
+     */
+    const ERROR_PATH_INVALID = 20;
+
+    /**
+     * Path does not exist while read() or isWritable() is called
+     */
+    const ERROR_PATH_DOES_NOT_EXIST = 30;
+
+    /**
+     * Cannot determine operation status because operation is async or unsafe
+     * or backend didn't returned anything
+     */
+    const STATUS_UNKNOWN = 40;
+
+    /**
+     * Get last error code
+     *
+     * @return int One of the StorageInterface::ERROR_* constant
+     */
+    public function getLastStatus();
+
     /**
      * Read a single value
      *
      * @param string $path      Path
      * @param int $expectedType ConfigType constant
      *
-     * @return mixed            Value
+     * @return mixed            Value if type matches and value exists
+     *                          else null
      */
     public function read($path, $expectedType);
 
@@ -48,7 +86,7 @@ interface StorageInterface
      * @param int $type    ConfigType constant
      * @param mixed $value Value
      * @param string $safe If set to false unsafe or asynchronous operations
-     *                     are allowed
+     *                     are allowed and key might now written
      */
     public function write($path, $type, $value, $safe = true);
 
@@ -58,6 +96,9 @@ interface StorageInterface
      * @param string|array $path If a string is given, it will be considered
      *                           as a single entry, if an array is given it
      *                           will be considered as a list of path
+     * @param string $safe       If set to false unsafe or asynchronous
+     *                           operations are allowed and key might not be
+     *                           deleted
      */
-    public function delete($path);
+    public function delete($path, $safe = true);
 }
